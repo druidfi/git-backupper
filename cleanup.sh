e84f67c0dff732e792933b22090ff7bc90a93d7b
Marko Korhonen <marko.korhonen@druid.fi>
2024-04-14 11:53:51 +0300

    Getting annoyed

--- cleanup.sh
+++ cleanup.sh
@@ -7,20 +7,20 @@ source utils.sh
 GH_OWNER=${GH_OWNER-"octocat"}
 GH_REPO=${GH_REPO-"git-backupper"}
 
-gh cache delete --all
+run gh cache delete --all
 
 # gh api repos/druidfi/git-backupper/actions/workflows | jq '.workflows[] | .id'
-workflow_ids=($(gh api repos/$GH_OWNER/$GH_REPO/actions/workflows | jq '.workflows[] | .id'))
+workflow_ids=($(run gh api repos/$GH_OWNER/$GH_REPO/actions/workflows | jq '.workflows[] | .id'))
 
 for workflow_id in "${workflow_ids[@]}"
 do
   info "- Listing runs for the workflow ID $workflow_id"
   # # gh api repos/druidfi/git-backupper/actions/workflows/51826721/runs --paginate | jq '.workflow_runs[].id'
-  run_ids=( $(gh api repos/$GH_OWNER/$GH_REPO/actions/workflows/$workflow_id/runs --paginate | jq '.workflow_runs[].id') )
+  run_ids=( $(run gh api repos/$GH_OWNER/$GH_REPO/actions/workflows/$workflow_id/runs --paginate | jq '.workflow_runs[].id') )
   for run_id in "${run_ids[@]}"
   do
     info "- Deleting Run ID $run_id"
     #gh api repos/$GH_OWNER/$GH_REPO/actions/runs/$run_id -X DELETE >/dev/null
-    gh run delete $run_id
+    run gh run delete $run_id
   done
 done
