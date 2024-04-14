283e11ecb50f983160a246f34209251789040d0d
Marko Korhonen <marko.korhonen@druid.fi>
2024-04-14 18:32:31 +0300

    owner got twice

--- cleanup.sh
+++ cleanup.sh
@@ -4,19 +4,18 @@ set -eu
 
 source utils.sh
 
-GH_OWNER=${GH_OWNER-"octocat"}
-GH_REPO=${GH_REPO-"git-backupper"}
+GH_REPO=${GH_REPO-"octocat/git-backupper"}
 
 run gh cache delete --all
 
 # gh api repos/druidfi/git-backupper/actions/workflows | jq '.workflows[] | .id'
-workflow_ids=($(run gh api repos/$GH_OWNER/$GH_REPO/actions/workflows | jq '.workflows[] | .id'))
+workflow_ids=($(run gh api repos/$GH_REPO/actions/workflows | jq '.workflows[] | .id'))
 
 for workflow_id in "${workflow_ids[@]}"
 do
   info "- Listing runs for the workflow ID $workflow_id"
   # # gh api repos/druidfi/git-backupper/actions/workflows/51826721/runs --paginate | jq '.workflow_runs[].id'
-  run_ids=( $(run gh api repos/$GH_OWNER/$GH_REPO/actions/workflows/$workflow_id/runs --paginate | jq '.workflow_runs[].id') )
+  run_ids=( $(run gh api repos/$GH_REPO/actions/workflows/$workflow_id/runs --paginate | jq '.workflow_runs[].id') )
   for run_id in "${run_ids[@]}"
   do
     info "- Deleting Run ID $run_id"
